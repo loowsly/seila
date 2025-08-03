2142639525756731033a37c7145b245b39253dda
loowsly <126037598+loowsly@users.noreply.github.com>
2025-08-03 17:35:14 -0300

    d

--- api/chat.js
+++ api/chat.js
@@ -8,9 +8,10 @@ function corsHeaders() {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Title, HTTP-Referer',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Title, HTTP-Referer, request-id',
   };
 }
+
 
 
 export default async function handler(req) {
